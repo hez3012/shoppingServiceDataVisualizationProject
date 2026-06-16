5d6929cd333f0e3dac1b8728d5ddcf4c09d5cc9d
Hezekiah Rico Mejilla <hrmejilla@gmail.com>
2026-06-16 11:08:34 +0800

    Revert Railway deployment changes

--- config/database.php
+++ config/database.php
@@ -56,10 +56,9 @@ return [
             'prefix'    => '',
             'strict'    => true,
             'engine'    => null,
-            'options' => [
-                Mysql::ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt',
-                Mysql::ATTR_SSL_VERIFY_SERVER_CERT => false,
-            ],
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
 
         'mariadb' => [

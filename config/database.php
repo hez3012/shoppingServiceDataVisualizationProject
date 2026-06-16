8cba25c1008c04a627c5cf887623e063dbaa3445
Hezekiah Rico Mejilla <hrmejilla@gmail.com>
2026-06-16 10:38:30 +0800

    Fix MySQL 9 SSL connection

--- config/database.php
+++ config/database.php
@@ -56,7 +56,8 @@ return [
             'prefix'    => '',
             'strict'    => true,
             'engine'    => null,
-            'options'   => [
+            'options' => [
+                PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt',
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ],
         ],
